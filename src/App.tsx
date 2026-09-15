617a01b02a144cc7f6f9ba88b11de89bbbe2b864
Rattan Saini <148332063+rattansaini@users.noreply.github.com>
2026-09-15 14:51:58 +0530

    Remove Register route and import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import OfflineBanner from './components/OfflineBanner';
 import UpdateNotification from './components/UpdateNotification';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
-import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import Dashboard from './pages/Dashboard';
@@ -45,7 +44,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route
